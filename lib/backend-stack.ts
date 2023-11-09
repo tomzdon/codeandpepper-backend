ab63fdf951fa7f019d45d7812a68205b1d9b65a7
tomaszwlodarczyk <tomekk.wlodarczyk@gmail.com>
2023-11-09 01:39:46 +0100

    added seeders and schema

--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -1,16 +1,48 @@
 import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Construct} from 'constructs';
+import * as path from 'path'
+import {AmplifyGraphqlApi, AmplifyGraphqlDefinition} from "@aws-amplify/graphql-api-construct";
+import {DynamoDBSeeder, Seeds} from '@cloudcomponents/cdk-dynamodb-seeder';
 
 export class BackendStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        const api = new AmplifyGraphqlApi(this, "SWAPI", {
+            apiName: "SWAPI",
+            definition: AmplifyGraphqlDefinition.fromFiles(
+                path.join(__dirname, "schema.graphql")
+            ),
+            authorizationModes: {
+                defaultAuthorizationMode: "API_KEY",
+                apiKeyConfig: {
+                    expires: cdk.Duration.days(30),
+                },
+            },
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const PersonTable = api.resources.tables['Person'];
+        const StarshipTable = api.resources.tables['Starship'];
+
+        const seedsPerson: Seeds = Seeds.fromInline([
+            {id: '1', name: 'Jan', birthYear: '1992-10-11', gender: 'Male', height: 180, mass: 3.14},
+            {id: '2', name: 'Tomasz', birthYear: '1992-07-11', gender: 'Male', height: 182, mass: 2.14},
+            {id: '3', name: 'Beata', birthYear: '1991-07-11', gender: 'Female', height: 172, mass: 4.14},
+        ]);
+
+        const seedsStarship: Seeds = Seeds.fromInline([
+            {id: '1', name: 'Altom', model: '12-1W', length: 10.2, crew: '3'},
+            {id: '2', name: 'FerGon', model: 'AC-1W', length: 18.2, crew: '2'},
+            {id: '3', name: 'SorGan', model: '2-1W', length: 1.2, crew: '1'},
+        ]);
+
+        new DynamoDBSeeder(this, 'InlineSeederPerson', {
+            table: PersonTable,
+            seeds: seedsPerson,
+        });
+        new DynamoDBSeeder(this, 'InlineSeederStarship', {
+            table: StarshipTable,
+            seeds: seedsStarship,
+        });
+    }
 }
